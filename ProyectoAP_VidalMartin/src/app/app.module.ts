5f71fad31c837259cedc557e4c992da01d4b5d11
martinmvidal <martinm.vidal@outlook.com>
2022-05-27 09:37:37 -0300

    Actualizado header y cargado Bootstrap

--- ProyectoAP_VidalMartin/src/app/app.module.ts
+++ ProyectoAP_VidalMartin/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoComponent } from './components/logo/logo.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoComponent
+    LogoComponent,
+    RedesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
