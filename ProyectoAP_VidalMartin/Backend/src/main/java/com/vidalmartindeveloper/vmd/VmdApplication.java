023acfb49774477804a78e3091a451e7d7c993dd
martinmvidal <martinm.vidal@outlook.com>
2022-06-23 09:44:27 -0300

    Comienzo de Backend

---
+++ ProyectoAP_VidalMartin/Backend/src/main/java/com/vidalmartindeveloper/vmd/VmdApplication.java
@@ -0,0 +1,13 @@
+package com.vidalmartindeveloper.vmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmdApplication.class, args);
+	}
+
+}
---
+++ ProyectoAP_VidalMartin/Backend/src/test/java/com/vidalmartindeveloper/vmd/VmdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidalmartindeveloper.vmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
